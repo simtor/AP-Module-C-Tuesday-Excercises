ba9a083f85d40a1fbc58ad7acbbfffa1472de0ac
Simone Ram <31472658+simtor@users.noreply.github.com>
2021-01-12 14:58:51 +0000

    Create Exercise 1.cpp

---
+++ Exercise 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char str[100];//declare a character array
+    int i,len,temp;
+    cout << "Hello, Welcome to ReverseWord\n";
+    cout << "Please enter a word: "; 
+    cin>>str; //input string
+    len=strlen(str);
+    i=0;
+    while(i<len/2){
+        temp=str[i];
+        str[i]=str[len-i-1];
+        str[len-i-1]=temp;
+ i++;
+    }
+   cout<<" spelt backwards is: "<<str;
+    
+    return 0;
+}
