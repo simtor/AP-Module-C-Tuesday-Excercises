50ea9c8176fd468e92eb51da7bba875f1cd25558
Simone Ram <31472658+simtor@users.noreply.github.com>
2021-01-20 11:32:14 +0000

    Added comments to the code

--- Exercise 2.cpp
+++ Exercise 2.cpp
@@ -1,29 +1,28 @@
-//LowerCase Converter
+//Exercise 2: LowerCase Converter
 
 #include <iostream>
 using namespace std;
 
 
 int main() {
-  string inputUser;
+  string inputUser;//declare the varible used for user Input
 
   cout << "Hello, Welcome to LowerCaseConverter\n";
   cout << "Please enter a word: ";
-  cin>> inputUser;
-  // cout<< inputUser<<"\n";
+  cin>> inputUser;//Getting user input by using cin
   string userInput=inputUser;
   string wordlength=userInput;
 
-  int listLen = wordlength.length();
+  int listLen = wordlength.length();//find the length of the varible 
   char c;
   char lowCaseLetters[listLen];
-
+  //for loop used to convert capital cases to  lower cases 
   for (int i =0; i <= listLen -1; i++){
     c = wordlength[i];
     lowCaseLetters[i] = (tolower(c));
     wordlength[i] = lowCaseLetters[i];
   }
-
+  //OUTPUT the result of the for loop 
   cout<< userInput<< " converted to lower case is: "<< wordlength;
   
 }
