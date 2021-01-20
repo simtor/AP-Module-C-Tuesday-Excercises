c1d4c7f1e6d7ab11d78b8fd1ac283504dfe9caca
Simone Ram <31472658+simtor@users.noreply.github.com>
2021-01-20 11:48:44 +0000

    Added comments to the code

--- Exercise 3.cpp
+++ Exercise 3.cpp
@@ -2,31 +2,33 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <algorithm>
 using namespace std;
 
 int main(){
+  //declare variables for the for loop.
     char string1[20];
     int i, length;
     int flag = 0;
     
-    cout << "Please enter a word:"; cin >> string1;
+    cout << "Please enter a word:";
+    cin >> string1;//Getting user input using cin 
+    length = strlen(string1);//Get the length of the user input 
     
-    length = strlen(string1);
-    
+    //Algorithm for finding the palindrome
     for(i=0;i < length ;i++){
         if(string1[i] != string1[length-i-1]){
             flag = 1;
-            break;
+            break;//breaks for loop whne finished 
    }
 }
+    //if statemnt used to  output if the user input is a palindrome or not 
     if (flag) {
         cout << "Sorry,"<< "'"<<string1 <<"'"<< " is not a palindrome" << endl; 
     }    
     else {
         cout << "Yes,"<< "'"<<string1 <<"'"<< " is a palindrome" << endl;  
     }
-    main();
+    main();//restart the function as show in the exmaple 
     return 0;
 }
 
